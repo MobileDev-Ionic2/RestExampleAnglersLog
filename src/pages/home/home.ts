6a664cc27561f9c499b3a4f7e2676676d8856e08
ebbevang <ev@evang.dk>
2017-04-03 13:19:13 +0200

    added post example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,10 +13,27 @@ import { Observable } from "rxjs/Observable";
 })
 export class HomePage {
 
+  public currentCatch:SimpleCatch = new SimpleCatch();
   public catches:Array<SimpleCatch> = new Array<SimpleCatch>();
 
   constructor(public navCtrl: NavController, public http:Http) {
+    http.get("api/catches")
+    .subscribe(
+      data => this.catches = data.json().catches);
+  }
+
+  public addCatch() 
+  {
+    this.currentCatch.latitude = 55.6427628;
+    this.currentCatch.longitude = 12.101314;
+    this.currentCatch.datetime = "2017-04-03 9:00:00";
+    alert(JSON.stringify(this.currentCatch));
+    this.http.post("api/catches", JSON.stringify(this.currentCatch))
+    .subscribe(
+      (val) => { alert("data succesfully send! " + val.text())},
+      (err) => { alert("No data received! " + err ) },
+      ()    => { alert("task completed?") }
+    )
     
-    http.get("api/catches").subscribe(data => this.catches = data.json().catches);
   }
 }
